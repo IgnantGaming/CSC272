bacd3b5d35108f80ae6a36c6d682e7f2f3bcfa20
IgnantGaming <54343011+IgnantGaming@users.noreply.github.com>
2021-04-08 19:33:51 -0700

    upload

---
+++ Week 1/Week1Live2B/src/CS272/SeanH/Main.java
@@ -0,0 +1,15 @@
+package CS272.SeanH;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+         //use comments to block out the sections below to see the runs.
+
+         //new StringConstructors();
+         //new StringMiscellaneous();
+
+        new stringCompare();
+    }
+}
